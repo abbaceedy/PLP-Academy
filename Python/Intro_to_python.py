3e921b499d7366785830823e4bd8efd82881bcd4
abbaceedy <163669034+abbaceedy@users.noreply.github.com>
2025-08-15 12:33:20 +0100

    Update Intro_to_python.py

--- Python/Intro_to_python.py
+++ Python/Intro_to_python.py
@@ -1,1 +1,14 @@
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+oper = input ('Enter operation (+, -, *, /): ')
 
+if oper == '+':
+  print(f'{num1} + {num2} = {num1 + num2 }')
+elif oper == '-':
+  print(f'{num1} - {num2} = {num1 - num2}')
+elif oper == '*':
+  print(f'{num1} * {num2} = {num1 * num2}')
+elif oper == '/':
+  print(f'{num1} / {num2} = {num1 / num2}' )
+else: 
+  print('Invalid Input')
