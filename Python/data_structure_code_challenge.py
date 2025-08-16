677e8b8759a59a71b1080226740251460d27b974
abbaceedy <163669034+abbaceedy@users.noreply.github.com>
2025-08-16 14:54:33 +0100

    Update data_structure_code_challenge.py

--- Python/data_structure_code_challenge.py
+++ Python/data_structure_code_challenge.py
@@ -1,1 +1,11 @@
+#Write a program that accepts user input to create a list of integers. Then, compute the sum of all the integers in the list.
+print('How many numbers do you want to enter? ')
+count = int(input)
 
+numbers = []
+for i in range(count):
+  num = int(input('Enter number: '))
+  numbers.append(num)
+
+print(numbers)
+print(sum(numbers))
