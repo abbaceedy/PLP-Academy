a3370806ce3326243cd1d7641054ef7f025de2dc
abbaceedy <163669034+abbaceedy@users.noreply.github.com>
2025-08-18 10:36:37 +0100

    Update dict_to_store_info.py

--- Python/data_structure_code_challenge/dict_to_store_info.py
+++ Python/data_structure_code_challenge/dict_to_store_info.py
@@ -1,1 +1,7 @@
+user_info = {}
 
+user_info['Name'] = input('Enter your name: ')
+user_info['Age'] = input('Enter your age: ')
+user_info['Favorite_color'] = input('Enter your favorite color: ')
+
+print(user_info)
