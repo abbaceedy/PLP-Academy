5900a993873d1654a4f4089154f8de6a6a71f027
abbaceedy <163669034+abbaceedy@users.noreply.github.com>
2025-08-19 13:03:15 +0100

    Update two_sets_of_integer.py

--- Python/data_structure_code_challenge/two_sets_of_integer.py
+++ Python/data_structure_code_challenge/two_sets_of_integer.py
@@ -1,1 +1,17 @@
+set1 = set()
+set2 = set()
+
+print ('Enter five integers')
+for i in range(5):
+  num = int(input())
+  set1.add(set1)
+
+print ('Enter another five integers')
+for i in range(5):
+  num2 = int(input())
+  set2.add(set2)
+
+sets_inter = (set1.intersection(set2))
+
+print(sets_inter)
 
