2737867a0f4c6e4d1f8fbda76998e772047bc9c0
abbaceedy <163669034+abbaceedy@users.noreply.github.com>
2025-08-19 13:47:23 +0100

    Update list_of_words.py

--- Python/data_structure_code_challenge/list_of_words.py
+++ Python/data_structure_code_challenge/list_of_words.py
@@ -1,1 +1,7 @@
+words = ['Python', 'Program', 'Code', 'Bug', 'Run', 'Execute']
 
+odd_word = [word for word in words if len(word) % 2 == 1]
+
+print (words)
+print('\nWords with odd number of characters: ')
+print (odd_word)
